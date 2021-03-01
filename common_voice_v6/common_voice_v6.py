a4162fbf00c985a65d1da90ad2a85e05822793a6
Jaidon-Smith <jsjsrobert500@gmail.com>
2021-03-01 11:03:21 +1100

    Update common_voice_v6.py

--- common_voice_v6/common_voice_v6.py
+++ common_voice_v6/common_voice_v6.py
@@ -98,9 +98,7 @@ class CommonVoiceV6(tfds.core.GeneratorBasedBuilder):
       print("The manual directory is: ", dl_manager.manual_dir)
     except:
       raise Exception("You have not defined the manual directoy where the .zip is located")
-    #archive_path = os.path.join(dl_manager.manual_dir, self.builder_config.language + '.tar')
-    #archive_path = os.path.join(dl_manager.manual_dir, 'ja.tar')
-    #archive_path = dl_manager.manual_dir
+
 
     # Extract the manually downloaded `data.zip`
     extracted_path = dl_manager.extract(archive_path)
